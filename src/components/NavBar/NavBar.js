9701034d3be3259d3e47747cafc6e65573ea233c
jcarbelbide <jcarbelbide@gmail.com>
2022-03-11 00:18:40 -0800

    Giving all credit for Navbar to maximakymenko on Github.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,6 +5,10 @@ import {Burger} from "./index";
 import Menu from "./Menu";
 import {useOnClickOutside} from "../../dependencies/util";
 
+// Most code borrowed from https://css-tricks.com/hamburger-menu-with-a-side-of-react-hooks-and-styled-components/
+// Git located here: https://github.com/maximakymenko/react-burger-menu-article-app
+// Code has been modified slightly, but all credit should go to maximakymenko on Github.
+// His Github profile found here: https://github.com/maximakymenko
 function NavBar(props) {
     const [navbarOpen, setNavbarOpen] = useState(false)
     const node = useRef()
